4fcb4451babe8c72701583523b65b1ac69446dce
neel_c <neel.c@latitudetechnolabs.com>
2017-01-24 00:25:28 +0530

    first commit

---
+++ app/src/main/java/com/example/qtm_c_android/firebasenotification/MyFirebaseInstanceIDService.java
@@ -0,0 +1,31 @@
+package com.example.qtm_c_android.firebasenotification;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by qtm-c-android on 30/11/16.
+ */
+
+public class MyFirebaseInstanceIDService  extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
+}
